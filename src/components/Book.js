739ae652e8a41d520a00671cd873fbf3377a734b
Mark Alexander <6917497+m15e@users.noreply.github.com>
2021-02-01 23:03:58 +0800

    ADD book component styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,16 +7,34 @@ const Book = props => {
   } = props;
 
   return (
-    <tr>
-      <th>{id}</th>
-      <th>{title}</th>
-      <th>{category}</th>
-      <th>
-        <button type="button" onClick={() => handleRemoveBook(id)}>
-          Remove
-        </button>
-      </th>
-    </tr>
+    <div className="book">
+      <div className="is-half" id={id}>
+       <div className="info">
+          <strong>{category}</strong>
+          <h3>{title}</h3>
+          <a className="author">Suzanne Collins</a>
+          <ul>
+            <li><a href="#">Comments</a></li>
+            <li><a href="#" onClick={() => handleRemoveBook(id)}>Remove</a></li>
+            <li><a href="#">Edit</a></li>
+          </ul>
+       </div>
+      </div>
+      <div className="is-half">
+        <div className="pct-col">
+          <div className="circle">
+           
+          </div>
+          <p className='pct-complete'>{Math.floor(Math.random()*99)}&#37;<span>Completed</span></p>
+          
+        </div>
+        <div className="progress-col">
+          <h5>Current Chapter</h5>
+          <p>Chapter 17</p>
+          <button type='button'>Update Progress</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Bookstore CMS</h1>
+      <a href="#" className="books">
+        Books
+      </a>
+      <a href="#" className="categories">
+        Categories
+      </a>
+    </div>
+  )
+}
+
+export default Header;
